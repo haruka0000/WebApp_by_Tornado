9b978e9999aedfd7d6ba875320007ae347007f97
Haruka Takahashi <h173335@edu.tut.ac.jp>
2019-01-15 16:56:46 +0900

    [add] vertical slide br

--- app.py
+++ app.py
@@ -3,19 +3,25 @@ import tornado.web
 import os
 import plot
 
+BASE_DIR = os.path.dirname(__file__)
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render("index.html", 
-                   graph_01=plot.graph_01()
-                  )
+        self.render("index.html")
+
+class HomeHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("home.html")
+
 
 def make_app():
     return tornado.web.Application(
         [
             (r"/", MainHandler),
+            (r"/home", HomeHandler),
         ],
-        template_path=os.path.join(os.getcwd(),  "templates"),
-        static_path=os.path.join(os.getcwd(),  "static"),
+        template_path=os.path.join(BASE_DIR, 'templates'),
+        static_path=os.path.join(BASE_DIR, 'static'),
     )
 
 if __name__ == "__main__":
